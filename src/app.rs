3b775965b2ef46feb49a70fbc4ac1b5192ea227b
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-10-27 19:22:29 +0100

    add: more pages/routes & screenshot swap

--- src/app.rs
+++ src/app.rs
@@ -27,6 +27,9 @@ pub fn App() -> impl IntoView {
                         <I18nRoute view=Outlet>
                             <Route path=path!("") view=AepaPage/>
                             <Route path=path!("/aepa") view=AepaPage/>
+                            <Route path=path!("/b") view=BPage/>
+                            <Route path=path!("/c") view=CPage/>
+                            <Route path=path!("/d") view=DPage/>
                         </I18nRoute>
                     </Routes>
                 </main>
@@ -50,6 +53,62 @@ fn AepaPage() -> impl IntoView {
 	<Title text=title/>
 
         <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+#[component]
+fn BPage() -> impl IntoView {
+    let i18n = use_i18n();
+
+    let q = "my-q-search-param";
+
+    let title = move || t_string!(i18n, auraa.meta_title, q = q);
+
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| count.update(|count| *count += 1);
+
+    view! {
+	<Title text=title/>
+
+        <h1>"B Page!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+#[component]
+fn CPage() -> impl IntoView {
+    let i18n = use_i18n();
+
+    let title = move || t_string!(i18n, putatoro.meta_title);
+
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| count.update(|count| *count += 1);
+
+    view! {
+	<Title text=title/>
+
+        <h1>"C Page!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+#[component]
+fn DPage() -> impl IntoView {
+    let i18n = use_i18n();
+
+    let title = move || t_string!(i18n, e404.meta_title);
+
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| count.update(|count| *count += 1);
+
+    view! {
+	<Title text=title/>
+
+        <h1>"D Page!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
